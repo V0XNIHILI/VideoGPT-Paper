3a2f00f925cc9a6b939e4318de6d2088d0096772
Douwe den Blanken <douwedb@gmail.com>
2024-05-03 00:05:16 +0200

    Only use 1 conditioning frame instead of 3 because that doesnt work

--- train_bair.sh
+++ train_bair.sh
@@ -1,1 +1,3 @@
-CUDA_VISIBLE_DEVICES=4,5 python train_videogpt.py -o ./bair_outputs --cfg gpt_small --vqvae_ckpt bair_stride4x2x2 --dataset bair_pushing --n_cond_frames 3 --batch_size 96 --total_iter 150000
+CUDA_VISIBLE_DEVICES=5 python train_videogpt.py -o ./bair_outputs --cfg gpt_small --vqvae_ckpt /space/ddenblanken/Projects/VideoGPT/bair_stride4x2x2 --dataset bair_pushing --n_cond_frames 1 --batch_size 8 --total_iter 150000
+
+CUDA_VISIBLE_DEVICES=5 python train_vqvae.py -o ./bair_outputs --cfg gpt_small --dataset bair_pushing --batch_size 8
