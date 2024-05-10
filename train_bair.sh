e3d57c56e7a7d00673784886a9fbc24c54332598
Douwe den Blanken <douwedb@gmail.com>
2024-05-10 14:12:29 +0200

    Update train script to use python3

--- train_bair.sh
+++ train_bair.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=5 python train_videogpt.py -o ./bair_outputs --cfg gpt_small --vqvae_ckpt /space/ddenblanken/Projects/VideoGPT/bair_stride4x2x2 --dataset bair_pushing --n_cond_frames 1 --batch_size 4 --total_iter 150000
+CUDA_VISIBLE_DEVICES=5 python3 train_videogpt.py -o ./bair_outputs --cfg gpt_small --vqvae_ckpt /space/ddenblanken/Projects/VideoGPT/bair_stride4x2x2 --dataset bair_pushing --n_cond_frames 1 --batch_size 4 --total_iter 150000
